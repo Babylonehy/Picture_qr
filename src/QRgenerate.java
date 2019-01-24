642b312b7b38aea91481554e63029defebbe0594
Sean <lixiangwhuerhy@gmail.com>
2019-01-24 11:19:53 +0800

    Refactor Version1.0

--- src/QRgenerate.java
+++ src/QRgenerate.java
@@ -4,7 +4,6 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -16,9 +15,9 @@ public class QRgenerate {
     private  String message;
     private  String name;
     private  String path;
-    final private  String QR_PATH="src/res/QR_";
-    Date time =new Date();
-    SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
+    private  String QR_PATH="src/res/QR_";
+    private Date time =new Date();
+    private SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
 
     /**
      *
@@ -27,13 +26,33 @@ public class QRgenerate {
      */
 
 
-      public  QRgenerate(String message, String name) {
+    QRgenerate(String message, String name, String QR_PATH) {
           this.message=message;
           this.name=name;
+          this.QR_PATH=QR_PATH;
 
         //输出目标文件
         File file = new File(QR_PATH+ft.format(time)+"/"+name);
         this.path=file.getPath();
+        generateQR(file);
+    }
+
+    QRgenerate(String message, String name){
+        this.message=message;
+        this.name=name;
+
+        //输出目标文件
+        File file = new File(QR_PATH+ft.format(time)+"/"+name);
+        this.path=file.getPath();
+        generateQR(file);
+
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    boolean generateQR(File file){
 
         if (!file.exists()) {
             try {
@@ -51,13 +70,11 @@ public class QRgenerate {
             BitMatrix bitMatrix = new MultiFormatWriter().encode(message, BarcodeFormat.QR_CODE,
                     300, 300, hints);// 生成矩阵
 
-           MatrixToImageWriter.writeToPath(bitMatrix, "png", file.toPath());// 输出图像
+            MatrixToImageWriter.writeToPath(bitMatrix, "png", file.toPath());// 输出图像
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public String getPath() {
-        return path;
+        return false;
     }
 }
